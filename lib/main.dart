552cef761031b9f1c2ce6987f9cd0e90ac011d7d
Fundevoge <59122389+Fundevoge@users.noreply.github.com>
2022-09-19 00:22:57 +0200

    Added Mission Statement

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,19 @@ void main() async {
   );
 
 }
+
+// TODO: Make Routines definable: Wie Wecker, teilweise auch mehrmals täglich oder Monatlich;
+//  Tagesplan mit subroutines; Template creation farblich anders
+// TODO: Groups with header; folded and unfolded
+// TODO: Sequential TODOS with constraints; Show additional info on what type of task
+//   e.g. calling somewhere  or working etc.
+// TODO: Menu for easily interacting with the Tds;
+//  -> Add Multiple tds at once with linebreaks
+// TODO: Make listitems interactive
+//  -> Move to next day or delete
+// TODO: Add deadlines / specific times to todos
+// TODO: Add Calendar view with apppointments
+// TODO: Make shared somehow
+//    Multiple Lists, when shared lists send requests
+//    option to move td to shared list
+// TODO: Fun things; relaxing views e.g. file transfer
