ccf4c53aae92fd00d04592d9eed2ff6d15b5d47f
Fundevoge <59122389+Fundevoge@users.noreply.github.com>
2022-09-20 22:03:13 +0200

    modified tasks.dart : function signatures moved tds to different file

--- lib/backend/tasks.dart
+++ lib/backend/tasks.dart
@@ -1,5 +1,14 @@
+import 'package:flutter/material.dart';
+
 void addTask(){}
 void markTaskDone(){}
 void markHeaderDone(){}
 List<dynamic> getTasks(DateTime day){return <dynamic>[];}
 List<dynamic> getAllTasks(){return <dynamic>[];}
+ListView buildTasksHeaders(bool Function(dynamic) whereF){return ListView();}
+
+bool onList(dynamic tasks, String listName) {return true;}
+
+class Task{
+  late final String name;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -24,31 +24,3 @@ void checkFirstOpen(){
   if(myPreferences.getInt('firstOpenedYear') != null) return;
   myPreferences.setInt('firstOpenedYear', DateTime.now().year);
 }
-
-// TODO: Make Routines definable: Wie Wecker, teilweise auch mehrmals täglich oder Monatlich;
-//  -> Tagesplan mit subroutines; Template creation farblich anders
-//  -> Long term Tds e.g. Stromvertrag
-// TODO: Groups with header
-//  -> folded and unfolded
-//  -> collapse daily routine to colored lines
-// TODO: Sequential TODOS with constraints; Show additional info on what type of task
-//   e.g. calling somewhere  or working etc.
-// TODO: Menu for easily interacting with the Tds;
-//  -> Add Multiple tds at once with linebreaks
-// TODO: Make listitems interactive
-//  -> Move to next day or delete
-// TODO: Add deadlines / specific times to todos
-// TODO: Create Pools of Tds/Things to do
-//  -> Can be pulled into plan without going away
-//  -> Extra pool for shared lists
-//  -> can be pulled from another pool?
-//  -> Task specific pools e.g. recipes for breakfast etc.
-// TODO: Add Calendar view with appointments
-//  -> show tasks from different groups in different colors
-// TODO: Make shared somehow
-//    Multiple Lists, when shared lists send requests
-//    option to move td between lists
-//    custom icons and names
-//    option for read-only-share; irgendwie ausgegraut
-// TODO: AUtomatic and manual backups
-// TODO: Fun things; relaxing views e.g. file transfer
