288133d41af067385040a92a66263800aaa3561b
Fundevoge <59122389+Fundevoge@users.noreply.github.com>
2022-09-21 00:07:55 +0200

    Set up classes that contain information on tasks except periodicity

--- lib/backend/tasks.dart
+++ lib/backend/tasks.dart
@@ -1,14 +1,71 @@
+/*
+TaskTypes:
+  template
+  unique
+  structure
+  periodic
+*/
+
 import 'package:flutter/material.dart';
 
-void addTask(){}
-void markTaskDone(){}
-void markHeaderDone(){}
-List<dynamic> getTasks(DateTime day){return <dynamic>[];}
-List<dynamic> getAllTasks(){return <dynamic>[];}
-ListView buildTasksHeaders(bool Function(dynamic) whereF){return ListView();}
+enum StructureTaskActive { always, workdays, holidays }
 
-bool onList(dynamic tasks, String listName) {return true;}
+class Periodicity {}
 
-class Task{
-  late final String name;
+class TemplateToH {
+  String name;
+  String notes;
+  Duration timeLimit;
+  List<TemplateToH>? children;
+  TemplateToH(this.name, this.notes, this.timeLimit, this.children);
+}
+
+class DoableToH extends TemplateToH {
+  String listName;
+  int index;
+  bool isDone;
+
+  DoableToH(name, notes, timeLimit, children, this.listName, this.index, this.isDone)
+      : super(name, notes, timeLimit, children);
+}
+
+class UniqueToH extends DoableToH {
+  DateTime dateTime;
+
+  UniqueToH(name, notes, timeLimit, children, listName, index, isDone, this.dateTime)
+      : super(name, notes, timeLimit, children, listName, index, isDone);
+}
+
+class StructureToH extends DoableToH {
+  StructureTaskActive whenActive;
+
+  StructureToH(name, notes, timeLimit, children, listName, index, isDone, this.whenActive)
+      : super(name, notes, timeLimit, children, listName, index, isDone);
+}
+
+class PeriodicToH extends DoableToH {
+  Periodicity periodicity;
+
+  PeriodicToH(name, notes, timeLimit, children, listName, index, isDone, this.periodicity)
+      : super(name, notes, timeLimit, children, listName, index, isDone);
+}
+
+
+void addTask() {}
+void markTaskDone() {}
+void markHeaderDone() {}
+List<dynamic> getTasks(DateTime day) {
+  return <dynamic>[];
+}
+
+List<dynamic> getAllTasks() {
+  return <dynamic>[];
+}
+
+ListView buildTasksHeaders(bool Function(dynamic) whereF) {
+  return ListView();
+}
+
+bool onList(dynamic tasks, String listName) {
+  return true;
 }
