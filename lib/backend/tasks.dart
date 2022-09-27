4ac3c1f9b242e1e0fdf47213e390c143bf2c479a
Fundevoge <59122389+Fundevoge@users.noreply.github.com>
2022-09-27 17:16:22 +0200

    Next iteration of task classes

--- lib/backend/tasks.dart
+++ lib/backend/tasks.dart
@@ -6,66 +6,125 @@ TaskTypes:
   periodic
 */
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:planer/backend/preference_manager.dart';
+import 'package:planer/backend/tasks.dart';
 
 enum StructureTaskActive { always, workdays, holidays }
 
-class Periodicity {}
+// 0 For Structure, 1 for repeating, 2 otherwise default
+final List<Color> colorsForTypes = [];
+void initColors() {
+  colorsForTypes.add(Color(myPreferences.getInt('structureTaskColor') ?? 0));
+  colorsForTypes.add(Color(myPreferences.getInt('repeatingTaskColor') ?? 0));
+  colorsForTypes.add(Color(myPreferences.getInt('defaultTaskColor') ?? 0));
+}
 
-class TemplateToH {
+class Periodicity {
+  List<Duration> baseOffsets;
+  List<Duration> rhythms;
+  DateTime baseDate;
+  Periodicity(this.baseDate, this.rhythms, this.baseOffsets);
+}
+
+enum TDConstraint { external, internal }
+
+// Any Task or Header:
+//  Name, Notes, timerduration?, subtasks?, listname, Date?, index, is_done, icon, color, isHighlighted, deadline?,
+//  is_repeating, constraints?
+class ToH {
   String name;
   String notes;
-  Duration timeLimit;
-  List<TemplateToH>? children;
-  TemplateToH(this.name, this.notes, this.timeLimit, this.children);
-}
-
-class DoableToH extends TemplateToH {
+  Duration? timeLimit;
+  List<ToH>? children;
   String listName;
+  DateTime? listDate;
   int index;
   bool isDone;
+  Icon icon;
+  int colorIndex;
+  bool isHighlighted;
+  DateTime? deadline;
+  bool isRepeating;
+  List<TDConstraint>? constraints;
 
-  DoableToH(name, notes, timeLimit, children, this.listName, this.index, this.isDone)
-      : super(name, notes, timeLimit, children);
+  ToH(
+      {required this.name,
+      required this.notes,
+      this.timeLimit,
+      this.children,
+      required this.listName,
+      this.listDate,
+      required this.index,
+      required this.isDone,
+      required this.icon,
+      required this.colorIndex,
+      required this.isHighlighted,
+      this.deadline,
+      required this.isRepeating,
+      this.constraints});
 }
-
-class UniqueToH extends DoableToH {
-  DateTime dateTime;
-
-  UniqueToH(name, notes, timeLimit, children, listName, index, isDone, this.dateTime)
-      : super(name, notes, timeLimit, children, listName, index, isDone);
-}
-
-class StructureToH extends DoableToH {
+// Structure (Not for task list, only backend and structure maniplator):
+//  Whenactive
+class StructureToH extends ToH {
   StructureTaskActive whenActive;
 
-  StructureToH(name, notes, timeLimit, children, listName, index, isDone, this.whenActive)
-      : super(name, notes, timeLimit, children, listName, index, isDone);
+  StructureToH(
+      {required super.name,
+      required super.notes,
+      super.timeLimit,
+      super.children,
+      required super.listName,
+      super.listDate,
+      required super.index,
+      required super.isDone,
+      required super.icon,
+      required super.colorIndex,
+      required super.isHighlighted,
+      super.deadline,
+      required super.isRepeating,
+      super.constraints,
+      required this.whenActive});
 }
-
-class PeriodicToH extends DoableToH {
+// Repeating (Not for task list, only backend and periodicity maniplator):
+//  Periodicity(Rhythm, Base_Date)
+class PeriodicToH extends ToH {
   Periodicity periodicity;
 
-  PeriodicToH(name, notes, timeLimit, children, listName, index, isDone, this.periodicity)
-      : super(name, notes, timeLimit, children, listName, index, isDone);
+  PeriodicToH(
+      {required super.name,
+        required super.notes,
+        super.timeLimit,
+        super.children,
+        required super.listName,
+        super.listDate,
+        required super.index,
+        required super.isDone,
+        required super.icon,
+        required super.colorIndex,
+        required super.isHighlighted,
+        super.deadline,
+        required super.isRepeating,
+        super.constraints,
+        required this.periodicity});
 }
-
 
 void addTask() {}
 void markTaskDone() {}
-void markHeaderDone() {}
-List<dynamic> getTasks(DateTime day) {
-  return <dynamic>[];
+List<ToH> getTasks(DateTime day) {
+  return <ToH>[];
 }
 
 List<dynamic> getAllTasks() {
-  return <dynamic>[];
+  return <ToH>[];
 }
 
-ListView buildTasksHeaders(bool Function(dynamic) whereF) {
+ListView renderToH(bool Function(ToH) whereF) {
   return ListView();
 }
 
-bool onList(dynamic tasks, String listName) {
+bool onList(List<ToH> tasks, String listName) {
   return true;
 }
