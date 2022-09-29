a6022bf51fc1f5286723cffb2bd969e4004761e0
Fundevoge <59122389+Fundevoge@users.noreply.github.com>
2022-09-29 22:39:49 +0200

    added global storage files and other task lists

--- lib/backend/persistance_manager.dart
+++ lib/backend/persistance_manager.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 import 'dart:convert';
+import 'dart:io';
 import 'package:planer/backend/tasks.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -7,6 +8,11 @@ void createTaskJson() {
   //db.execute("CREATE TABLE todoListNames(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, UNIQUE(name)");
   //db.execute("CREATE TABLE tohs(uid INTEGER PRIMARY KEY, name TEXT, notes TEXT, timeLimit INT, ");
 }
+
+late final File taskListFile;
+late final File structureTaskFile;
+late final File repeatingTaskFile;
+late final File templateTaskFile;
 
 final Map<String, LinkedHashMap<DateTime, List<ToH>>> todoLists = {};
 void initTodoListsDebug() {
@@ -46,4 +52,8 @@ void initTodoLists(String encoded) {
       })
   });
 }
+
+final List<StructureToH> structureToHs = [];
+final List<PeriodicToH> periodicToHs = [];
+final List<ToH> templateToHs = [];
 
